8e980224dfa257e64f3c1d17ff6897f51a9d3c46
Oringik <zikalol2@mail.ru>
2020-03-12 15:49:03 +0500

    asdasd

--- goserver/models/rating_org_config.go
+++ goserver/models/rating_org_config.go
@@ -8,8 +8,8 @@ import (
 type RatingOrgConfig struct {
 	gorm.Model
 	OrganizationID uint          `gorm:"column:org_id;unique"`
-	StartTime      uint8         `gorm:"column:start_time;default:17"`
-	WeekDay        time.Weekday  `gorm:"column:week_day;default:0"`
+	StartTime      uint8         `gorm:"column:start_time;default:16"`
+	WeekDay        time.Weekday  `gorm:"column:week_day;default:3"`
 	Organization   *Organization `gorm:"foreignkey:org_id"`
 }
 
